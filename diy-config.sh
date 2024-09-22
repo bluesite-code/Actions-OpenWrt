d7636adb5d994e381dcc58c1a54ada068a111b95
bluesite-code <bluesite@outlook.com>
2024-09-23 00:57:52 +0800

    Update diy-config.sh

--- diy-config.sh
+++ diy-config.sh
@@ -13,6 +13,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.61.1/g' package/base-files/files/bin/config_generate
 
+# Changes password
+password=$(openssl passwd -1 'admin')
+sed -i "s|root::0:0:99999:7:::|root:$password:0:0:99999:7:::|g" package/base-files/files/etc/shadow
+
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
