354973b7467370b85fff5e3377360c1e23e48dab
bluesite-code <bluesite@outlook.com>
2024-09-23 10:32:15 +0800

    Update diy-config.sh

--- diy-config.sh
+++ diy-config.sh
@@ -14,12 +14,12 @@
 sed -i 's/192.168.1.1/192.168.61.1/g' package/base-files/files/bin/config_generate
 
 # Changes password
-password=$(openssl passwd -1 'admin')
-sed -i "s|root::0:0:99999:7:::|root:$password:0:0:99999:7:::|g" package/base-files/files/etc/shadow
+#password=$(openssl passwd -1 'admin')
+#sed -i "s|root::0:0:99999:7:::|root:$password:0:0:99999:7:::|g" package/base-files/files/etc/shadow
 
 # Modify default theme
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
