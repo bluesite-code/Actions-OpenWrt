bfbc69d435a4553e40d2a5dfb92265ea4f803f01
bluesite-code <bluesite@outlook.com>
2024-09-26 23:24:29 +0800

    Update diy-plug-fros.sh

--- diy-plug-fros.sh
+++ diy-plug-fros.sh
@@ -18,8 +18,9 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add packages
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 
 #Add alist&mosdns
 #rm -rf feeds/packages/lang/golang
@@ -29,6 +30,6 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git https://github.com/sbwml/v2ray-geodata package/v2ray-geodata' >> feeds.conf.default
 
 # Add theme
-#echo 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
-#cd openwrt/package
-#echo 'src-git https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
+echo 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+cd openwrt/package
+echo 'src-git https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
