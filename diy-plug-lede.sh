7e68cae2fe2a718120715fb22d1eabe961c5aa43
bluesite-code <bluesite@outlook.com>
2024-09-26 21:49:13 +0800

    Update diy-plug-lede.sh

--- diy-plug-lede.sh
+++ diy-plug-lede.sh
@@ -21,8 +21,8 @@
 echo 'src-git fros https://github.com/destan19/fros-packages-openwrt.git;fros-23.05' >>feeds.conf.default
 
 #Add alist&mosdns
-rm -rf feeds/packages/lang/golang
-echo 'src-git https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang' >> feeds.conf.default
+#rm -rf feeds/packages/lang/golang
+#echo 'src-git https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang' >> feeds.conf.default
 echo 'src-git https://github.com/sbwml/luci-app-alist package/alist' >> feeds.conf.default
 echo 'src-git https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns' >> feeds.conf.default
 echo 'src-git https://github.com/sbwml/v2ray-geodata package/v2ray-geodata' >> feeds.conf.default
