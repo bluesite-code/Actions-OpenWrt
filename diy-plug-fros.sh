af5c487495db3b8544433a744cbccc1ed2a401a8
bluesite-code <bluesite@outlook.com>
2024-09-27 01:24:18 +0800

    Update diy-plug-fros.sh

--- diy-plug-fros.sh
+++ diy-plug-fros.sh
@@ -20,7 +20,7 @@
 # Add packages
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+#echo 'src-git packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 
 #Add alist&mosdns
 #rm -rf feeds/packages/lang/golang
