fbc6574edbfef6c0ddeb5a39b4d9e54cc522601f
bluesite-code <bluesite@outlook.com>
2024-09-27 09:24:01 +0800

    Update diy-plug-lede.sh

--- diy-plug-lede.sh
+++ diy-plug-lede.sh
@@ -16,12 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add fros
 echo 'src-git fros https://github.com/destan19/fros-packages-openwrt.git;fros-23.05' >>feeds.conf.default
-
-# Add packages
-echo 'src-git packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 
 # Add alist&mosdns
 #rm -rf feeds/packages/lang/golang
