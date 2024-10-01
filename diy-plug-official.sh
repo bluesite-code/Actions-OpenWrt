427c22860e044af44558bc7398d62c886ee73029
bluesite-code <bluesite@outlook.com>
2024-10-01 12:29:57 +0800

    Update diy-plug-official.sh

--- diy-plug-official.sh
+++ diy-plug-official.sh
@@ -21,11 +21,10 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add alist&mosdns
-#sudo rm -rf feeds/packages/lang/golang
-#echo 'src-git https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang' >> feeds.conf.default
-#echo 'src-git https://github.com/sbwml/luci-app-alist package/alist' >> feeds.conf.default
-#echo 'src-git https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns' >> feeds.conf.default
-#echo 'src-git https://github.com/sbwml/v2ray-geodata package/v2ray-geodata' >> feeds.conf.default
+#rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 
 # Add theme
 #echo 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
