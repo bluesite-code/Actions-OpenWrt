4c988deb04482a97c3e14a54279775aaf928c9ab
bluesite-code <bluesite@outlook.com>
2024-10-17 13:46:04 +0800

    Update diy-plug-official.sh

--- diy-plug-official.sh
+++ diy-plug-official.sh
@@ -25,13 +25,12 @@
 git clone https://github.com/bluesite-code/fros -b fros-23.05 package/fros
 
 # Add alist&mosdns
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf feeds/packages/lang/golang
+#rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-alist package/alist
-#find package/*/ -maxdepth 3 -name "Makefile" | xargs sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g'
 
 # Add other
 #git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
