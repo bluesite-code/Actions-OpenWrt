ea42bbbc0c0cc56c4b13fcf487511adbbdd4851a
bluesite-code <bluesite@outlook.com>
2024-10-21 15:40:02 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 添加自定义源和软件包
+
+# 克隆 fros 项目
+git clone https://github.com/bluesite-code/fros -b fros-23.05 package/fros
+
+# 克隆 luci-theme-argon 主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# 克隆 luci-app-fileassistant 文件助手应用
+git clone https://github.com/kenzok78/luci-app-fileassistant package/luci-app-fileassistant
+
+# 克隆 luci-app-alist 及其依赖
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone https://github.com/sbwml/openwrt-alist package/openwrt-alist
+
+# 克隆 luci-app-mosdns 及其依赖
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 如果需要,可以在这里添加更多自定义命令
+echo "自定义软件包下载完成"
