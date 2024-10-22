bc77d5c85d263af2501fc639d3c08fb28fbe16ce
bluesite-code <bluesite@outlook.com>
2024-10-22 15:50:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,14 +12,14 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 git clone https://github.com/kenzok78/luci-app-fileassistant package/luci-app-fileassistant
 
 # 克隆 luci-app-alist 及其依赖
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist package/alist
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # 克隆 luci-app-mosdns 及其依赖
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#rm -rf feeds/packages/net/v2ray-geodata
+#git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 如果需要,可以在这里添加更多自定义命令
 echo "自定义软件包下载完成"
