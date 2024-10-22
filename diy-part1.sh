0dd11eeb76016f55bf02ddbf6a1a03866b8921a5
bluesite-code <bluesite@outlook.com>
2024-10-22 09:41:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,12 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 git clone https://github.com/kenzok78/luci-app-fileassistant package/luci-app-fileassistant
 
 # 克隆 luci-app-alist 及其依赖
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # 克隆 luci-app-mosdns 及其依赖
+rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
