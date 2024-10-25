3189edccd99a3111d3de379f30fdf1fedeecfdcb
bluesite-code <bluesite@outlook.com>
2024-10-26 00:43:32 +0800

    Update diy-plug-official.sh

--- diy-plug-official.sh
+++ diy-plug-official.sh
@@ -16,17 +16,13 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+#echo 'src-git packages https://github.com/kiddin9/kwrt-packages.git' >>feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 
 # Add fros
 git clone https://github.com/bluesite-code/fros -b fros-23.05 package/fros
-
-# Update go.mod file
-#echo "module $(go list -m)" > go.mod && go mod edit -go 1.23.2
-#go mod tidy
 
 # Add alist&mosdns
 rm -rf feeds/packages/lang/golang
