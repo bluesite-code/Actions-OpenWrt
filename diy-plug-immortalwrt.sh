1aa21c556f948ca33a5ccc1197bf43641032f47d
bluesite-code <bluesite@outlook.com>
2025-02-23 20:27:50 +0800

    Update diy-plug-immortalwrt.sh

--- diy-plug-immortalwrt.sh
+++ diy-plug-immortalwrt.sh
@@ -32,14 +32,13 @@ rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 #git clone https://github.com/wixxm/WikjxWrt-golang feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # Add other
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
-#git clone https://github.com/wangqn/luci-app-filebrowser package/luci-app-filebrowser
 git clone https://github.com/kenzok78/luci-app-fileassistant package/luci-app-fileassistant
 
 # Add theme
@@ -49,6 +48,6 @@ rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 
 # 添加FUSE相关包
-#git clone https://github.com/openwrt/packages -b openwrt-24.10 trd/packages
-#cp -rf trd/packages/utils/fuse feeds/packages/utils/
-#rm -rf trd
+git clone https://github.com/openwrt/packages -b openwrt-24.10 trd/packages
+cp -rf trd/packages/utils/fuse feeds/packages/utils/
+rm -rf trd
