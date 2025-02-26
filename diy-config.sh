9ccb509610e4f7579f3458e0f810ea7928d27dae
bluesite-code <bluesite@outlook.com>
2025-02-27 00:07:38 +0800

    Update diy-config.sh

--- diy-config.sh
+++ diy-config.sh
@@ -10,6 +10,11 @@
 # See /LICENSE for more information.
 #
 
+# Add Golang 23.x support
+cd /workdir/openwrt
+echo "CONFIG_GOLANG_VERSION=\"23.x\"" >> .config
+echo "CONFIG_GOLANG_EXTERNAL_BOOTSTRAP_ROOT=\"/usr\"" >> .config
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.61.1/g' package/base-files/files/bin/config_generate
 
